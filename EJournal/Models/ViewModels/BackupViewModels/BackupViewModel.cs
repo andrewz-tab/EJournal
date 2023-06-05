1e26a2ad6a4fae8dd7edf9ed0c37182f21d3b09c
andrewz-tab <55842156+andrewz-tab@users.noreply.github.com>
2023-06-05 12:07:51 +0700

    addbackup

---
+++ EJournal/Models/ViewModels/BackupViewModels/BackupViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EJournal.Models.ViewModels.BackupViewModels
+{
+    public class BackupViewModel
+    {
+        public string statusMessage { get; set; }
+        public bool isError { get; set; } = false;
+        public BackupViewModel(string statusMessage, bool isError)
+        {
+            this.statusMessage = statusMessage;
+            this.isError = isError;
+        }
+    }
+}
